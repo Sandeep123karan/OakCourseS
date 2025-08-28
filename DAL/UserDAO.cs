a50685b00ec47b54b92028dcc9eb911717e814e6
Sandeep123karan <sandeepkaran234@gmail.com>
2025-08-28 13:25:13 +0530

    Add project files.

---
+++ DAL/UserDAO.cs
@@ -0,0 +1,44 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UserDAO:PostContext
+    {
+        public UserDTO GetUserWithUsernameAndPassword(UserDTO model)
+        {
+            UserDTO dto = new UserDTO();
+            T_User user = db.T_User.FirstOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+            if(user!=null && user.ID != 0)
+            {
+                dto.ID = user.ID;
+                dto.Username = user.Username;
+                dto.Name = user.NameSurname;
+                dto.Imagepath = user.ImagePath;
+                dto.isAdmin = user.isAdmin;
+            }
+            return dto;
+        }
+        public int AddUser(T_User user)
+        {
+            try
+            {
+                db.T_User.Add(user);
+                db.SaveChanges();
+                return user.ID; 
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
---
+++ UI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using BLL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        UserBLL bll = new UserBLL();
+        
+        // GET: Admin/User
+        public ActionResult AddUser()
+        {
+            UserDTO model= new UserDTO();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult AddUser(UserDTO model)
+        {
+            if (model.UserImage==null)
+            {
+                ViewBag.ProcessState = General.Messages.ImageMissing;
+            }
+            else if (ModelState.IsValid)
+            {
+                string filename = "";
+                HttpPostedFileBase postedfile = model.UserImage;
+                Bitmap UserImage = new Bitmap(postedfile.InputStream);
+                Bitmap resizeimage = new Bitmap(UserImage, 128, 128);
+                string ext = Path.GetExtension(postedfile.FileName);
+                if(ext==".jpg"|| ext==".jpeg" || ext==".png" || ext == ".gif")
+                {
+                    string uniqueNumber = Guid.NewGuid().ToString();
+                    filename = uniqueNumber + postedfile.FileName;
+                    resizeimage.Save(Server.MapPath("~/Areas/Admin/Content/UserImage/" + filename));
+                    model.Imagepath = filename;
+                    bll.AddUser(model); 
+                    
+
+                }
+                else
+                {
+                    ViewBag.ProcessState = General.Messages.ImageMissing;
+                }
+            }
+            else
+            {
+                ViewBag.ProcessState = General.Messages.EmptyArea;
+            }
+            return View(model);
+        }
+    }
+}
